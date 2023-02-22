c9eac8da486af7f9de464b89bf57fe7e5aabfa98
Fatalis <connman2400@gmail.com>
2023-02-22 15:59:30 -0600

    resolvers query and mutation(addUser)

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,3 +2,24 @@ const { User } = require('../models/User');
 const { signToken } = require('../utils/auth');
 const { AuthenticationError } = require('apollo-server-express');
 
+const resolvers = {
+    Query:{
+        me: async (parent, args, context) =>{
+            if (context.user) {
+                const data = await User.findOne({
+                    _id: context.user._id
+                }).select('-__v -password');
+                return data;
+            }
+            throw new AuthenticationError('Log in is Required');
+        }
+    },
+
+    Mutation:{
+        addUser: async (parent, args) =>{
+            const user = await User.create({args});
+            const token = signToken(user);
+            return { token, user};
+        }
+    },
+}
