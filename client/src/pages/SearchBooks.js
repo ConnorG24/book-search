b066c32969f812ec4217d9cc32f5f6bf3934ddaf
Fatalis <connman2400@gmail.com>
2023-02-26 21:23:12 -0600

    handlesaveBook changes

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -20,7 +20,7 @@ const SearchBooks = () => {
   useEffect(() => {
     return () => saveBookIds(savedBookIds);
   });
-
+const [saveBook] = useMutation(SAVE_BOOK);
   // create method to search for books and set state on form submit
   const handleFormSubmit = async (event) => {
     event.preventDefault();
@@ -66,7 +66,7 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const response = await saveBook({variables: {input: {...bookToSave}}});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
