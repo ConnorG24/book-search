2a8efe8c97f52cd29ac4237ec30b5aa2d2d307ad
Fatalis <connman2400@gmail.com>
2023-02-26 20:34:04 -0600

    required imports

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/mutations';
+import {useMutation} from '@apollo/client'
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
